b559027c06137633bfe2ba831dcae0c9ffca2428
Nico Olivera <oliveran9@gmail.com>
2023-02-01 19:53:09 -0300

    inicializando repo

---
+++ src/screens/ProductsScreen.jsx
@@ -0,0 +1,31 @@
+import { Button, StyleSheet, Text, View } from "react-native"
+import React, { useEffect } from "react"
+import ProductsItem from "../components/ProductsItem"
+
+const ProductsScreen = ({ navigation, route }) => {
+  useEffect(() => {
+    console.log(route)
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.productsContainer}>
+        <ProductsItem />
+      </View>
+    </View>
+  )
+}
+
+export default ProductsScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  productsContainer: {
+    height: 150,
+    width: 150,
+  },
+})
